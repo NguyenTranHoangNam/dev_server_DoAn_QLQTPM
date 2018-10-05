3c63da6ec13b6cdde433556ab325a4c5cdf9fb6f
Tran The Ngoc <thengoc@imgroup.vn>
2018-10-05 10:51:57 +0700

    update process run server and please read file README.md

--- server_QLQTPM/server.js
+++ server_QLQTPM/server.js
@@ -1,6 +1,23 @@
 ﻿var http = require('http');
-var port = process.env.port || 1337;
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+var num_port = 1742;
+var port = process.env.port || num_port;
+var server = http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write("<!DOCTYPE html>");
+    res.write("<html>");
+    res.write("<head>");
+    res.write("<meta charset='utf-8'>");
+    res.write("<title>Hello World Page</title>");
+    res.write("</head>");
+    res.write("<body>");
+    res.write("Xin chào");
+    res.write("tôi cà ai nhỉ");
+    res.write("</body>");
+    res.write("</html>");
+    res.end();
+    console.log("day la file log");
+});
+
+server.listen(port);
+console.log("Running server!!!");
+console.log("Link running: localhost:" + process.env.port + " and localhost:" + num_port);
