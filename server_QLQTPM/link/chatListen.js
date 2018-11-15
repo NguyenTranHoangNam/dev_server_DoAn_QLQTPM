738233284b61259e7edfc8da1696f4421e8cd473
theNgoc <thengoc@imgroup.vn>
2018-11-15 22:04:59 +0700

    update code socket & file demo

--- server_QLQTPM/link/chatListen.js
+++ server_QLQTPM/link/chatListen.js
@@ -12,11 +12,11 @@ var listActing = [];
 var listRoom = [];
 var listClient = [];
 wss.on('connection', function(socket) {
-	console.log(`${socket.id} ket noi toi`);
+	console.log(`${socket.m_name} ket noi toi`);
 
 	if(typeof socket.m_name == "undefined") {
 		socket.emit('check-name');
-		console.log(socket.id + " re-check name.");
+		console.log(socket.m_name + " re-check name.");
 	}
 
 	// event khi client ngắt kết nối với server socket
@@ -31,38 +31,37 @@ wss.on('connection', function(socket) {
 
 	// khi client gửi tên hiển thị (hay tên hiển thị,...) cho server 
 	socket.on('login', function(email) {
+		console.log(email+" logging in");
 		// khi phát hiện đã phát hiện trùng tên client (có ng khác login thì cho out)
 		if(listActing.indexOf(email)>=0){
 			// console.log(email + " logged in false");
 			socket.emit("login-error");
 		} else{
-			db.addUser(email)
-			.then(info => {
-				console.log('add user success');
-			})
-			.catch(error => {
-				console.log('user found in data');
-			});
-
 			socket.m_name = email;
 			listActing.push(email);
 			listClient.push(socket);
 			socket.emit("login-success");
 			wss.sockets.emit("list-user", listActing);
-			
+			var me = require('../process/chatProcess');
 			db.getListRoomOfUser(email)
 			.then(rows => {
-				var list = JSON.stringify(rows)
-				//console.log(Object.keys(list).length + list);
-				if( list.length!=undefined){
-					for (var i = list.length - 1; i >= 0; i--) {
-						if(list[i].room_name == null){
-							list[i].room_name = db.getNameRoomWhenRoomNullName(list[i].room_id, socket.m_name);
+				if(rows.length!=undefined){
+					for (var i = rows.length - 1; i >= 0; i--) {
+						if(rows[i].room_name == ""){
+							me.getNameRoomWhenRoomNullName(rows[i].room_id, socket.m_name)
+							.then(value => {
+								console.log("string receive convert null " + value);
+							})
+							.catch(err => {
+								console.log(err);
+							});
+							console.log(`name changed `+rows[i].room_name);
 						}
 
-						socket.join(list[i].room_id);
+						socket.join(rows[i].room_id);
 					}
-					socket.emit('list-room', list);
+					console.log(rows);
+					socket.emit('list-room', rows);
 				}
 				//console.log(JSON.stringify(rows));
 			})
@@ -84,12 +83,13 @@ wss.on('connection', function(socket) {
 
 	// khi client gửi tin nhắn cho server gồm các thông tin id_room, message
 	socket.on('client-sent', function(infoMessage) {
-		if(message.length == 0) return;
-		db.checkUserInRoom(infoMessage.id_room, infoMessage.message)
+		console.log("receive messenger: "+infoMessage);
+		if(infoMessage.message == "") return;
+		db.checkUserInRoom(infoMessage.id_room, socket.m_name)
 		.then(row => {
 			if(row.length ==1){
 				wss.sockets.in(socket.m_room).emit('server-sent',{name: socket.m_name, 'message': infoMessage.message});
-				db.saveMessage(infoMessage.id_room,socket.m_name,infoMessage.message)
+				db.saveMessage(infoMessage.id_room, socket.m_name, infoMessage.message)
 				.then(status => { })
 				.catch(error => console.log(error));
 			} 
@@ -97,23 +97,23 @@ wss.on('connection', function(socket) {
 		// console.log(`${socket.m_name}: ${message}`);
 	});
 
-	// khi gửi yêu cầu tạo room chat thì gửi trước danh sách người cùng chat
+	// khi gửi yêu cầu tạo room chat thì gửi trước danh sách JSON người cùng chat
 	socket.on('add-room', (listUser) => {
 		console.log("Request add room with name "+listUser);
-		
-		db.createRoom(list-user)
+		let list = JSON.parse(lisUser);
+		db.createRoom(list)
 		.then(value => {
 			console.log("added room with key: "+value.insertId);
 			socket.join(value.insertId)
 
-			for (var j = listUser.length - 1; j >= 0; j--) {
-				db.addUserToRoom(value.insertId, listUser[j])
+			for (var j = list.length - 1; j >= 0; j--) {
+				db.addUserToRoom(value.insertId, list[j])
 				.catch(error => {
 					console.log("Error when add user to room");
 				});
 
-				for (var i = 0; i < listClient.length; i++) {
-					if(listClient[i].m_name == listUser[j])
+				for (var i = 0; i < list.length; i++) {
+					if(listClient[i].m_name == list[j])
 						listClient[i].join(value.insertId);
 				}
 			}
@@ -135,33 +135,78 @@ wss.on('connection', function(socket) {
 	socket.on("get_history_message", (room_id) => {
 		socket.m_room = room_id;
 		db.updateDateSeenRoomOfUser(room_id, socket.m_name);
-		socket.emait("server-get-message-in-room", JSON.stringify(db.getAllMessageInRoom(room_id)));
+		db.getAllMessageInRoom(room_id)
+		.then(rows => {
+			socket.emit("server-get-message-in-room", rows);
+		})
+		.catch(err => {
+			console.log(err);
+		})
 	});
 
 	socket.on("get_history_with", (email_partner) => {
 		db.getRoomWithPartner(socket.m_name, email_partner)
 		.then(row =>{
+			console.log(JSON.stringify(row));
 			if(row.length == 1){
 				socket.m_room = row[0].id_room;
 				db.updateDateSeenRoomOfUser(socket.m_room, socket.m_name);
-				socket.emait("server-get-message-in-room", JSON.stringify(db.getAllMessageInRoom(socket.m_room)));
+				socket.emit("server-get-message-in-room", JSON.stringify(db.getAllMessageInRoom(socket.m_room)));
 			}else if(row.length == 0){
-				db.createRoom("")
+				db.createRoom(socket.m_name+", "+email_partner)
 				.then(value => {
 					console.log("added room with key: "+value.insertId);
 					socket.join(value.insertId)
 
 					db.addUserToRoom(value.insertId, email_partner)
+					.then(val => {
+						console.log(`Add user ${email_partner} to room ${value.insertId} SUCCESS`);
+					})
 					.catch(error => {
-						console.log("Error when add user to room");
+						console.log(`Error when add user ${email_partner} to room ${value.insertId}`);
 					});
 
-					listClient[i].join(value.insertId);
+					db.addUserToRoom(value.insertId, socket.m_name)
+					.then(val => {
+						console.log(`Add user ${socket.m_name} to room ${value.insertId} SUCCESS`);
+					})
+					.catch(error => {
+						console.log(`Error when add user ${socket.m_name} to room ${value.insertId}`);
+					});
+					db.saveMessage(value.insertId,socket.m_name,socket.m_name+" create this room")
+					.catch(error => {
+						console.log(`Error when add user ${socket.m_name} to room ${value.insertId}`);
+					});
+					for (var i = 0; i < listClient.length; i++) {
+						if(listClient[i].m_name == email_partner)
+							listClient[i].join(value.insertId);
+					}
+				})
+				.catch(error => {
+					console.log(error);
+				});
+				db.getListRoomOfUser(email_partner)
+				.then(rows => {
+					var list = JSON.stringify(rows)
+					if( list.length!=undefined){
+						for (var i = list.length - 1; i >= 0; i--) {
+							if(list[i].room_name == null){
+								list[i].room_name = db.getNameRoomWhenRoomNullName(list[i].room_id, socket.m_name);
+							}
+
+							socket.join(list[i].room_id);
+						}
+						socket.emit('list-room', list);
+					}
+
 				})
 				.catch(error => {
 					console.log(error);
 				});
 			}
+		})
+		.catch(error => {
+			console.log(error);
 		});
 	});
 
