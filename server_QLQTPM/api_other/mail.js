8897edb0025b6d535ec60896c2a4d6dcdaaad4d6
theNgoc <thengoc@imgroup.vn>
2018-11-25 14:59:23 +0700

    get info mail receive mail from database

--- server_QLQTPM/api_other/mail.js
+++ server_QLQTPM/api_other/mail.js
@@ -48,123 +48,119 @@ exports.sendMail = function(infomation,res) {
   });
 }
 
-var info_mail_admin = {};
-db.load("select Email, PasswordMail, PostImap, HostImap from AccountCompany where Username like 'supportcentermanagement'")
-.then(row => {
-  if(row.length == 1){
-    info_mail_admin.mail = row[0].Email;
-    info_mail_admin.pass = row[0].PasswordMail;
-    info_mail_admin.post = row[0].PostImap;
-    info_mail_admin.host = row[0].HostImap;
-  }
-})
-.catch(err => {
-  console.log(err); 
-});
-
-var imap = new Imap({
-  user: info_mail_admin.mail || 'htkh17hcb@gmail.com',
-  password: info_mail_admin.pass || '0908325568',
-  host: info_mail_admin.host || 'imap.gmail.com',
-  port: info_mail_admin.post || 993,
-  tls: true,
-  keepalive: {
-    interval: 3000,
-    idleInterval: 3000,
-    forceNoop: true
-  },
-  tlsOptions: { rejectUnauthorized: false },
-  connTimeout: 10000,  
-  authTimeout: 5000,   
-    mailbox: "INBOX", // mailbox to monitor 
-    searchFilter: ["UNSEEN", "FLAGGED"], // the search filter being used after an IDLE notification has been retrieved 
-    markSeen: true, 
-    fetchUnreadOnStart: true, 
-  });
-
-// function mo? hop thu de doc mail
-function openInbox(cb) {
-  imap.openBox('INBOX', false, cb);
-};
-
-// function dung de duyet cac mail trong hop thu
-
-function fetchMessages2(imap){
-
-  imap.search(['UNSEEN'], function(err, results){
-    if(err)console.log('you are already up to date');
-    else {
-      var f = imap.fetch(results,{ bodies:'',struct: true,markSeen: true});
-      f.on('message', function(msg, seqno){
-
-        var prefix = '(#' + seqno + ') ';
-        var parser = new MailParser();
-       //"INSERT INTO Mail (`Subject`, `Content`, `Assigner`, `SendTime`) VALUES ('" + mail.subject + "', '" + mail.text.toString() + "', '" + mail.from.value[0].address.toString() + "', '" + mail.date.toString() + "');";
-       msg.on('body', function(stream, info){
-        simpleParser(stream, (err, mail) => {
-          if(mail!=null)
-          {
-            var cut=mail.text.toString().indexOf("Vào");
-            var content=mail.text.toString().replace(/[\r\n]/g, ' ');
-            if(cut>0)
-            {
-              content=mail.text.toString().slice(0,cut).replace(/[\r\n]/g, ' ');
-            }
-
-            db.write( "INSERT INTO Mail (`Subject`, `Content`, `Email`, `SendTime`,`InReplyTo`) VALUES ('" + mail.subject + "', '" + content + "', '" + mail.from.value[0].address.toString() + "', '" + mail.date.toString() + "','" + mail.inReplyTo + "')")
-            .then(value=>
-            {
-              console.log("insert susccess!!");
-            })
-            .catch(err=>{
-              console.log("insert error!!"+err);
-            });
-            console.log(prefix +" tieu de: "+ mail.subject);
-            console.log(prefix +" email from: "+ mail.from.value[0].address);
-            console.log(prefix +" thoi gian: "+ mail.date);
-
-            console.log(prefix +" noi dung: "+content); 
-            console.log(prefix +"inReplyTo: "+ mail.inReplyTo);
-            console.log(prefix +"messageId: "+ mail.messageId);
-            console.log(mail.references);
-            console.log(mail.replyTo);
-            
-          }
-
-        });
-      });
-     })
-
-      f.once('end', function() {
-        console.log('Done fetching all messages!');
-      });
-      
-      f.once('error', function(err) {
-        console.log('Fetch error: ' + err);
-      });
-    }
-  });
+function formatDate(date) {
+  var year = date.getFullYear(),
+  month = date.getMonth() + 1, // months are zero indexed
+  day = date.getDate(),
+  hour = date.getHours(),
+  minute = date.getMinutes(),
+  second = date.getSeconds();
+  return year + "-" +month + "-" + day + " " +  hour + ":" + minute + ":" + second;
 }
-
 
 exports.receiveMail = function(req,res) {
-
-  imap.once('ready', function() {
-    openInbox(function(err, box) {
-      if (err) throw err;
-      imap.on('mail', function(numNewMsgs){
-        console.log(numNewMsgs + " messages has arrived");
-        fetchMessages2(imap);
-      });
-
+  db.load("select Email, PasswordMail, PostImap, HostImap from AccountCompany where Username like 'supportcentermanagement'")
+  .then(row => {
+    var imap = new Imap({
+      user: row[0].Email || 'htkh17hcb@gmail.com',
+      password: row[0].PasswordMail || '0908325568',
+      host: row[0].PostImap || 'imap.gmail.com',
+      port: row[0].HostImap || 993,
+      tls: true,
+      keepalive: {
+        interval: 3000,
+        idleInterval: 3000,
+        forceNoop: true
+      },
+      tlsOptions: { rejectUnauthorized: false },
+      connTimeout: 10000,  
+      authTimeout: 5000,   
+      mailbox: "INBOX", // mailbox to monitor 
+      searchFilter: ["UNSEEN", "FLAGGED"], // the search filter being used after an IDLE notification has been retrieved 
+      markSeen: true, 
+      fetchUnreadOnStart: true, 
     });
+
+    // function mo? hop thu de doc mail
+    function openInbox(cb) {
+      imap.openBox('INBOX', false, cb);
+    };
+
+    // function dung de duyet cac mail trong hop thu
+
+    function fetchMessages2(imap){
+      imap.search(['UNSEEN'], function(err, results){
+        if(err)console.log('you are already up to date');
+        else {
+          var f = imap.fetch(results,{ bodies:'',struct: true,markSeen: true});
+          f.on('message', function(msg, seqno){
+
+            var prefix = '(#' + seqno + ') ';
+            var parser = new MailParser();
+            //"INSERT INTO Mail (`Subject`, `Content`, `Assigner`, `SendTime`) VALUES ('" + mail.subject + "', '" + mail.text.toString() + "', '" + mail.from.value[0].address.toString() + "', '" + mail.date.toString() + "');";
+            msg.on('body', function(stream, info){
+              simpleParser(stream, (err, mail) => {
+                if(mail!=null)
+                {
+                  var cut=mail.text.toString().indexOf("Vào");
+                  var content=mail.text.toString().replace(/[\r\n]/g, ' ');
+                  if(cut>0)
+                  {
+                    content=mail.text.toString().slice(0,cut).replace(/[\r\n]/g, ' ');
+                  }
+
+                  db.write( "INSERT INTO Mail (`Subject`, `Content`, `Email`, `SendTime`,`InReplyTo`) VALUES ('" + mail.subject + "', '" + content + "', '" + mail.from.value[0].address.toString() + "', '" + formatDate(mail.date) + "','" + mail.inReplyTo + "')")
+                  .then(value=>
+                  {
+                    console.log("insert susccess!!");
+                  })
+                  .catch(err=>{
+                    console.log("insert error!!"+err);
+                  });
+                  console.log(prefix +" tieu de: "+ mail.subject);
+                  console.log(prefix +" email from: "+ mail.from.value[0].address);
+                  console.log(prefix +" thoi gian: "+ mail.date);
+
+                  console.log(prefix +" noi dung: "+content); 
+                  console.log(prefix +"inReplyTo: "+ mail.inReplyTo);
+                  console.log(prefix +"messageId: "+ mail.messageId);
+                  console.log(mail.references);
+                  console.log(mail.replyTo);
+
+                }
+
+              });
+            });
+          })
+
+          f.once('end', function() {
+            console.log('Done fetching all messages!');
+          });
+
+          f.once('error', function(err) {
+            console.log('Fetch error: ' + err);
+          });
+        }
+      });
+    }
+    imap.once('ready', function() {
+      openInbox(function(err, box) {
+        if (err) throw err;
+        imap.on('mail', function(numNewMsgs){
+          console.log(numNewMsgs + " messages has arrived");
+          fetchMessages2(imap);
+        });
+
+      });
+    })
+
+    imap.once('error', function(err) {
+      console.log(err);
+    });
+
+    imap.connect();
   })
-
-  imap.once('error', function(err) {
-    console.log(err);
+  .catch(err => {
+    console.log(err); 
   });
-  
-  imap.connect();
-
 }
-
